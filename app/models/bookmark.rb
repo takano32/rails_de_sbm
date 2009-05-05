7904538786544b40e310fbcc74c267b22da97eaa
TAKANO Mitsuhiro <takano32@gmail.com>
2009-05-05 15:32:53 +0900

    modify for Rails 2.3.2

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,4 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :user
-  belongs_to :page  validates_uniqueness_of :page_id, :scope => :user_id
+  belongs_to :page #validates_uniqueness_of :page_id, :scope => :user_id
 end
